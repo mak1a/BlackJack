078db191971cd1e4926bea7d6604397997ef94b4
makia <g1821120@gmail.com>
2020-04-21 11:35:35 +0900

    改良完了

---
+++ Game/Game/src/Main.cpp
@@ -0,0 +1,59 @@
+﻿# include "Common.hpp"
+# include "Title.hpp"
+# include "Game.hpp"
+# include "Bet.hpp"
+# include "Result.hpp"
+# include "Score.hpp"
+
+void Main() {
+	// （ウィンドウの閉じるボタンを押したときだけ終了する場合はコメントを外す）
+	//System::SetTerminationTriggers(UserAction::CloseButtonClicked);
+
+	Window::Resize(1280, 720);
+
+	// タイトルを設定
+	Window::SetTitle(U"Black Jack");
+
+	// 背景色を設定
+	Scene::SetBackground(ColorF(0.4, 0.7, 0.5));
+
+	// 使用するフォントアセットを登録
+	FontAsset::Register(U"Title", 120, Typeface::Bold, FontStyle::Italic);
+	FontAsset::Register(U"Menu", 30, Typeface::Regular);
+	FontAsset::Register(U"Version", 14, Typeface::Regular);
+	FontAsset::Register(U"Button", 30, Typeface::Light);
+	FontAsset::Register(U"Result", 50, Typeface::Bold);
+	FontAsset::Register(U"Resultcount", 28, Typeface::Regular);
+	FontAsset::Register(U"betMoneyTheme", 50, Typeface::Bold);
+	FontAsset::Register(U"ScoreList", 50, Typeface::Heavy);
+	FontAsset::Register(U"ScoreListDate", 25, Typeface::Regular, FontStyle::Italic);
+	FontAsset::Register(U"PlayerScore", 50, Typeface::Bold);
+	FontAsset::Register(U"GameResult", 120, Typeface::Regular);
+	FontAsset::Register(U"KeyEnterDown", 35, Typeface::Regular);
+	FontAsset::Register(U"betMoney", 70, Typeface::Regular);
+	FontAsset::Register(U"betMoneyExplain", 35, Typeface::Regular);
+
+	TextureAsset::Register(U"Deck", Resource(U"texture/cardgame_deck_hiku.png"));
+	TextureAsset::Register(U"CardMan", Resource(U"texture/cardgame_card_dasu.png"));
+
+	// シーンと遷移時の色を設定
+	MyApp manager;
+	manager
+		.setFadeColor(Palette::Black)
+		.add<Title>(State::Title)
+		.add<Bet>(State::Bet)
+		.add<Game>(State::Game)
+		.add<Result>(State::Result)
+		.add<Score>(State::Score)
+		.setFadeColor(ColorF(1.0));
+
+	// （ゲームシーンから開始する場合はコメントを外す）
+	//manager.init(State::Game);
+
+	while (System::Update()) {
+		if (!manager.update()) {
+			break;
+		}
+	}
+}
+
---
+++ Game/Game/src/Score.cpp
@@ -0,0 +1,50 @@
+﻿#include "Score.hpp"
+
+Score::Score(const InitData& init)
+: IScene(init)
+, m_highScores({
+    ScoreData{0, Date(2020,1,1)},
+    ScoreData{0, Date(2020,1,1)},
+    ScoreData{0, Date(2020,1,1)},
+    ScoreData{0, Date(2020,1,1)},
+    ScoreData{0, Date(2020,1,1)}
+}) {
+    if (FileSystem::Exists(Resource(U"Save/Score.dat"))) {
+        Deserializer<BinaryReader>{Resource(U"Save/Score.dat")}(m_highScores);
+    }
+    else {
+        Serializer<BinaryWriter>{Resource(U"Save/Score.dat")}(m_highScores);
+    }
+}
+
+void Score::update() {
+    // タイトル画面に戻る処理
+    if (MouseL.down()) {
+        changeScene(State::Title);
+    }
+}
+
+void Score::draw() const {
+    const int32 h = FontAsset(U"ScoreList").height();
+    
+    for (const auto [i, highScore] : IndexedRef(m_highScores))
+    {
+        const Rect rect = Rect(520, 100).setCenter(Scene::CenterF().x + 100, static_cast<int>(120 + i * 120));
+        
+        rect.draw(ColorF(1.0, 0.2));
+        
+        FontAsset(U"ScoreList")(i + 1, U"位").draw(252, rect.pos.y + (rect.h - h) / 2 + 2, Palette::Gray);
+        FontAsset(U"ScoreList")(i + 1, U"位").draw(250, rect.pos.y + (rect.h - h) / 2, Palette::Black);
+        
+        FontAsset(U"ScoreList")(highScore.score)
+        .draw(rect.pos + Point(42, (rect.h - h) / 2 + 2), Palette::Gray);
+        
+        FontAsset(U"ScoreList")(highScore.score)
+        .draw(rect.pos + Point(40, (rect.h - h) / 2), Palette::Black);
+        
+        const Size dateSize = FontAsset(U"ScoreListDate")(highScore.date).region().size;
+        
+        FontAsset(U"ScoreListDate")(highScore.date)
+        .draw(rect.br().x - dateSize.x - 40, rect.center().y - dateSize.y / 2, Palette::Black);
+    }
+}
